c8e988024e06d2144b95bb2141be522fe778d898
github username <github email address>
2020-02-27 23:20:06 -0600

    update

--- create-phone-number.js
+++ create-phone-number.js
@@ -5,16 +5,26 @@ function createPhoneNumber(numbers) {
 	let phoneNumber = `(${part1}) ${part2}-${part3}`;
 
 	for (let i = 0; i < 4; i++) {
-		part1 += numbers[i];
+		part1 = [...part1, numbers[i]];
 	}
 
-	for (let j = 4; j < 7; j++) {
-		part2 += numbers[j];
+	for (let j = 3; j < 6; j++) {
+		part2 = [...part2, numbers[j]];
 	}
 
-	for (let k = 0; k < 4; k++) {
-		part3 += numbers[k];
+	for (let k = 6; k < 9; k++) {
+		part3 = [...part3, numbers[k]];
 	}
 
 	return phoneNumber;
+}
+
+function createPhoneNumber(numbers) {
+	var format = "(xxx) xxx-xxxx";
+
+	for (var i = 0; i < numbers.length; i++) {
+		format = format.replace("x", numbers[i]);
+	}
+
+	return format;
 }
