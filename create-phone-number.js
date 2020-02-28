934c0a0a9df1ff16586097ce6328ee895fe84534
github username <github email address>
2020-02-27 23:23:54 -0600

    test1

--- create-phone-number.js
+++ create-phone-number.js
@@ -18,13 +18,3 @@ function createPhoneNumber(numbers) {
 
 	return phoneNumber;
 }
-
-function createPhoneNumber(numbers) {
-	var format = "(xxx) xxx-xxxx";
-
-	for (var i = 0; i < numbers.length; i++) {
-		format = format.replace("x", numbers[i]);
-	}
-
-	return format;
-}
